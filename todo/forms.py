fe94da58b65282f31ed240096638f52bbebe1bf3
sacrac <crocha09.09@gmail.com>
2012-11-27 22:07:55 -0600

    Listo formulario de afuera con formset

--- todo/forms.py
+++ todo/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
-from todo.models import *
+from todo.models import Todo, TodoArticulo
 
 class TodoForm(ModelForm):
 	class Meta:
 		model = Todo
 
+class TodoArticuloForm(ModelForm):
+	class Meta:
+		model = TodoArticulo
+		exclude = ('fktodo',)
--- todo/views.py
+++ todo/views.py
@@ -2,24 +2,46 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
-from todo.models import Todo, TodoArticulo
-from todo.forms import *
+from todo.models import Todo
+from todo.forms import TodoForm, TodoArticuloForm
+from django.forms.formsets import formset_factory, BaseFormSet
 
 def index(request):
 
+	# See http://stackoverflow.com/questions/2406537/django-formsets-make-first-required/4951032#4951032
+	class RequiredFormSet(BaseFormSet):
+		def __init__(self, *args, **kwargs):
+			super(RequiredFormSet, self).__init__(*args, **kwargs)
+			for form in self.forms:
+				form.empty_permitted = False
+
+	#si se fija aca le puse extra 3 para que me pinte 3 formularios de tareas al mismo tiempo
+	#si se lo quito pues pone solo uno prueben 
+	TodoArticuloFormSet = formset_factory(TodoArticuloForm, extra=3, max_num=10, formset=RequiredFormSet)
+	
+
 	if request.method == 'POST':
+
 		todo_form = TodoForm(request.POST)
-		if todo_form.is_valid():
-			todo_form.save()
+		todo_articulo_formset = TodoArticuloFormSet(request.POST, request.FILES)
+
+		if todo_form.is_valid() and todo_articulo_formset.is_valid():
+			todo_lista = todo_form.save()
+			for form in todo_articulo_formset.forms:
+				todo_item = form.save(commit=False)
+				todo_item.fktodo = todo_lista
+				todo_item.save()
+
 			return HttpResponseRedirect('/')
 	else:
 		todo_form = TodoForm()
+		todo_articulo_formset = TodoArticuloFormSet()
 
 	todo = Todo.objects.all().order_by('id') #Select * from Todo ;
-	return  render_to_response('index.html',
-		                       RequestContext(request, locals()))
+	return  render_to_response('index.html', locals(),
+								RequestContext(request))
 
 def ver_detalle(request, id):
 	ver = get_object_or_404(Todo, id=id)
-	return render_to_response('todo/ver.html',
-		                      RequestContext(request, locals()))
+	return render_to_response('todo/ver.html', {'ver': ver},
+								RequestContext(request))
